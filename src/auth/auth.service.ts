26416b3f4264202901ef8a4509b9c886ebf7fcfc
kenier <kenier202@gmail.com>
2024-12-09 14:47:59 -0500

    feat(AuthService): add implementation to create

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
-
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './entities/user.entity';
+import { Model } from 'mongoose';
+import * as bcrypt from 'bcryptjs';
 @Injectable()
 export class AuthService {
-  create(CreateUserDto: CreateUserDto) {
+
+  constructor(
+    @InjectModel(User.name) private userModel: Model<User>)
+    {}
+
+  async create(CreateUserDto: CreateUserDto): Promise<User> {
     console.log(CreateUserDto);
-    return 'This action adds a new auth';
+    try{
+      const {password, ...userdata} = CreateUserDto;
+      const newUser = new this.userModel({
+        password: bcrypt.hashSync(password, 10),
+        ...userdata
+      });
+      await newUser.save();
+      const {password:_, ...user} = newUser.toJSON();
+      return user;
+      
+    }catch(e){
+      if (e.code === 11000){
+        throw new BadRequestException(`${CreateUserDto.email} already exists`);
+      }
+      throw new InternalServerErrorException('Internal server error');
+    }
+
+
   }
 
   findAll() {
